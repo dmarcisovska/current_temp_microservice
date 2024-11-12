e0974f4b3d64f9d14ac35da4f584cf96f7a3769e
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-11-11 16:08:14 -0800

    updated port to 3008

--- publisher.js
+++ publisher.js
@@ -3,8 +3,8 @@ const zmq = require("zeromq");
 async function run() {
   const sock = new zmq.Publisher();
 
-  await sock.bind("tcp://127.0.0.1:3000");
-  console.log("Publisher bound to port 3000");
+  await sock.bind("tcp://127.0.0.1:3008");
+  console.log("Publisher bound to port 3008");
 
   while (true) {
     console.log("sending a multipart message envelope");
