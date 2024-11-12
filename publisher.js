ca5c4eb8a89bf6da58e711d3dd9740ab65754b4a
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-11-11 16:16:09 -0800

    creation of env file with weather api key

--- publisher.js
+++ publisher.js
@@ -1,4 +1,18 @@
-const zmq = require("zeromq");
+import zmq from "zeromq";
+import axios from "axios";
+
+async function getTemperature(lat, lon) {
+  const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  try {
+    const response = await axios.get(url);
+    const temp = response.data.main.temp;
+    return `Current temperature: ${temp}°C`;
+  } catch (error) {
+    console.error("Error fetching temperature data:", error);
+    return "Temperature data unavailable";
+  }
+}
 
 async function run() {
   const sock = new zmq.Publisher();
